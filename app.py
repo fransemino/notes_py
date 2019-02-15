9bfaa1910f28325ed76fe4fb08e1df83e2aeed91
fransemino <47389204+fransemino@users.noreply.github.com>
2019-02-15 10:56:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ if __name__ == '__main__':
     with app.app_context():
         CORS(app)
         db.create_all()
-        app.run(host='localhost', debug=True)
+        app.run(host='0.0.0.0', debug=True)
